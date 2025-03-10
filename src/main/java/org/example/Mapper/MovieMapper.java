5c58b5420e14d9f06c1381356eb7bb66f62f1f9e
Erik Svensson Nokrach <74262686+kirre02@users.noreply.github.com>
2025-03-10 18:18:20 +0100

    feat: Implement a mapper

--- src/main/java/org/example/Mapper/MovieMapper.java
+++ src/main/java/org/example/Mapper/MovieMapper.java
@@ -1,4 +1,27 @@
 package org.example.Mapper;
 
+import org.example.Entity.Movie;
+import org.example.dto.CreateMovie;
+import org.example.dto.MovieResponse;
+
+
 public class MovieMapper {
+    private MovieMapper() {}
+
+    public static MovieResponse map(Movie movie) {
+        if (movie == null) return null;
+        return new MovieResponse(movie.getId(), movie.getTitle(), movie.getDirector(), movie.getDuration(), movie.getYear());
+    }
+
+    public static Movie map(CreateMovie movie) {
+        if (movie == null) return null;
+
+        Movie newMovie = new Movie();
+        newMovie.setTitle(movie.title());
+        newMovie.setDirector(movie.director());
+        newMovie.setDuration(movie.duration());
+        newMovie.setYear(movie.year());
+
+        return newMovie;
+    }
 }
